8c2b74e7868c4acb32c46192e8cc3e4de5bd388e
Rahulsehlot <rahulsehelot@gmail.com>
2022-07-11 14:17:18 +0530

    Changes aplied

--- src/Scenes/progress_bar/progress_bar.js
+++ src/Scenes/progress_bar/progress_bar.js
@@ -15,69 +15,71 @@ export default function Star({ num }) {
 
   return (
     <>
-      <Image
-        src={Assets?.scene8?.sprites[3]}
-        alt="txt"
-        className="progress_bar"
-      />
+      <div className="star-wrapper">
+        <Image
+          src={Assets?.scene8?.sprites[3]}
+          alt="txt"
+          className="progress_bar"
+        />
 
-      <div className="wrap_1" style={{ position: "fixed", right: "0%" }}>
-        <Image
-          src={
-            num >= 1 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 1 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 2 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 2 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 3 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 3 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 4 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 4 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 5 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 5 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 6 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 6 ? "star" : ""}
-        />
-        <Image
-          src={
-            num >= 7 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
-          }
-          alt="txt"
-          id="stars"
-          className={num === 7 ? "star" : ""}
-        />
+        <div className="star-wrapper0">
+          <Image
+            src={
+              num >= 1 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 1 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 2 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 2 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 3 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 3 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 4 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 4 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 5 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 5 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 6 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 6 ? "star" : ""}
+          />
+          <Image
+            src={
+              num >= 7 ? Assets?.scene8?.sprites[5] : Assets?.scene8?.sprites[4]
+            }
+            alt="txt"
+            id="stars"
+            className={num === 7 ? "star" : ""}
+          />
+        </div>
       </div>
     </>
   );
